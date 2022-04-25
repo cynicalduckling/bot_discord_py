42a6cc4995f3a402db32d7cd22432740e6d26d6f
rohithseby <48560429+rohithseby@users.noreply.github.com>
2022-04-25 15:35:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from discord.ext import commands
 
 
 def main():
+
+    token = "OTY4MDc1MjM0MjExMTU1OTk5.YmZkUQ.yj0vgERZCDfMoqfEnFo-AMVqxDE"
+    
     bot = commands.Bot(command_prefix = ".") 
 
     @bot.event
     async def on_ready():
         print("Connected to Discord")
 
-    bot.run("OTY4MDc1MjM0MjExMTU1OTk5.YmZkUQ.yj0vgERZCDfMoqfEnFo-AMVqxDE")
+    bot.run(token)
 
 if __name__ == '__main__':
     main()
